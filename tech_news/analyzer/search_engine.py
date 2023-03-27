2d39d6519620afccd3ce1eb36617397dddd17916
Solon <amaral.solon@hotmail.com>
2023-03-27 15:58:54 -0300

    Req 7 - Search By Title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,28 @@
 # Requisito 7
+# from tech_news.database import db
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # resolvendo com append e search_news
+    search_criteria = {"title": {"$regex": title, "$options": "i"}}
+    news = search_news(search_criteria)
+    response = []
+    for new in news:
+        response.append((new["title"], new["url"]))
+    return response
+
+    # resolvendo com retorno inserido no for loop
+    # news = search_news({"title": {"$regex": f"{title}", "$options": "i"}})
+    # return [(new["title"], new["url"]) for new in news]
+
+    # resolvendo com método find direto no db
+    # tuplas = []
+    # news = db.news.find()
+    # for new in news:
+    #     if title.lower() in new["title"].lower():
+    #         tuplas.append((new["title"], new["url"]))
+    # return tuplas
 
 
 # Requisito 8
