5a064e23ecbae7a8c51d7cfb0ae397bc1b44d108
Solon <amaral.solon@hotmail.com>
2023-03-27 16:29:05 -0300

    Req 8 - Search by Date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,7 @@
 # Requisito 7
 # from tech_news.database import db
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -27,7 +28,18 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        # https://docs.python.org/3/library/datetime.html?highlight=fromisoformat#datetime.date.fromisoformat
+        # https://docs.python.org/3/library/datetime.html?highlight=strftime#datetime.datetime.strftime
+        date_converted = datetime.fromisoformat(date).strftime('%d/%m/%Y')
+    except ValueError:
+        raise ValueError('Data inválida')
+    search_criteria = {"timestamp": date_converted}
+    response = []
+    news = search_news(search_criteria)
+    for new in news:
+        response.append((new["title"], new["url"]))
+    return response
 
 
 # Requisito 9
