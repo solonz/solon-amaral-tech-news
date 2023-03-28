f8a0b646c7d33b62648b90a3b736f0470faffed6
Solon <amaral.solon@hotmail.com>
2023-03-27 23:18:38 -0300

    Req 10 - Top 5 Categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,21 @@
+from tech_news.database import find_news
+import pandas as pd
+
+
 # Requisito 10
+# https://datagy.io/python-count-occurrences-in-list/
+# https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
+# https://towardsdatascience.com/3-ways-to-count-the-item-frequencies-in-a-python-list-89975f118899
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = []
+    news = find_news()
+
+    for new in news:
+        categories.append(new["category"])
+
+# https://pandas.pydata.org/docs/reference/api/pandas.Series.value_counts.html?highlight=value_counts#pandas.Series.value_counts
+    ranking = pd.Series(categories).value_counts().sort_index()
+
+# https://www.w3schools.com/python/pandas/ref_df_nlargest.asp
+# https://www.geeksforgeeks.org/python-pandas-series-tolist/
+    return ranking.nlargest(5).index.tolist()
