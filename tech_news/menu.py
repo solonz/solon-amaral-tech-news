8791d32d3524fdf3baf4af3ff888aab2e86bcd83
Solon <amaral.solon@hotmail.com>
2023-03-29 21:13:33 -0300

    Reqs 11 e 12 - User Menu - Pegadinha hein... espaços antes das opções me deixaram horas travado

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,59 @@
+import sys
+from tech_news.analyzer.ratings import top_5_categories
+from tech_news.analyzer.search_engine import search_by_category
+from tech_news.analyzer.search_engine import search_by_date
+from tech_news.analyzer.search_engine import search_by_title
+from tech_news.scraper import get_tech_news
+
+
 # Requisitos 11 e 12
+def news_qtt():
+    qtt = input("Digite quantas notícias serão buscadas:")
+    print(get_tech_news(qtt))
+
+
+def search_title():
+    title = input("Digite o título:")
+    print(search_by_title(title))
+
+
+def search_date():
+    date = input("Digite a data no formato aaaa-mm-dd:")
+    print(search_by_date(date))
+
+
+def search_category():
+    category = input("Digite a categoria:")
+    print(search_by_category(category))
+
+
+# def saida():
+#     print("Encerrando script")
+
+
+# https://computinglearner.com/how-to-create-a-menu-for-a-python-console-application/
+# https://stackoverflow.com/questions/19964603/creating-a-menu-in-python
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    user_input = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por categoria;\n"
+        " 4 - Listar top 5 categorias;\n"
+        " 5 - Sair.\n"
+    )
+
+    options = {
+        "0": news_qtt,
+        "1": search_title,
+        "2": search_date,
+        "3": search_category,
+        "4": lambda: top_5_categories(),
+        "5": lambda: print("Encerrando script"),
+    }
+
+    try:
+        print(options[user_input]())
+    except KeyError:
+        sys.stderr.write("Opção inválida\n")
